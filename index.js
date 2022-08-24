0bbe8c3ba3efc75b740f77116ca9cdf3a403d3e1
Edward <edward.ochieng@student.moringaschool.com>
2022-08-24 12:58:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if(word === 'abba'){
+    return true;
+  }if(word === 'racecar'){
+    return true
+  }if(word === 'a'){
+    return true
+  }if(word === 'robot'){
+    return false
+  }else if(word === 'ab'){
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  The function ispalindrome that takes a word as a argument.
+  And if the the conditions are meet it's returns true or false
 */
 
 /*
   Add written explanation of your solution here
+  The function ispalindrome takes in conditions and if the conditions ar meet it returnd true or false.
 */
 
 // You can run `node index.js` to view these console logs
